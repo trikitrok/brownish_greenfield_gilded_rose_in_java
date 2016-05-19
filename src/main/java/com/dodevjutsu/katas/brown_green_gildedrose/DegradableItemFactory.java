b6c2504d6ccd88260d84a6feb5fc5b39e06d7b11
trikitrok <trikitrok@gmail.com>
2016-05-19 22:27:56 +0200

    refactor: extracted several explaining methods

--- src/main/java/com/dodevjutsu/katas/brown_green_gildedrose/DegradableItemFactory.java
+++ src/main/java/com/dodevjutsu/katas/brown_green_gildedrose/DegradableItemFactory.java
@@ -2,13 +2,25 @@ package com.dodevjutsu.katas.brown_green_gildedrose;
 
 public class DegradableItemFactory {
     public static DegradableItem create(Item item) {
-        if (item.name.equals("Aged Brie")) {
+        if (isAgedBrie(item)) {
             return new AgedBrie(item);
-        } else if (item.name.equals("Sulfuras")) {
+        } else if (isSulfuras(item)) {
             return new Sulfuras(item);
-        } else if(item.name.equals("Backstage passes")) {
+        } else if(areBackstagePasses(item)) {
             return new BackstagePasses(item);
         }
         return new RegularItem(item);
+    }
+
+    private static boolean areBackstagePasses(Item item) {
+        return item.name.equals("Backstage passes");
+    }
+
+    private static boolean isSulfuras(Item item) {
+        return item.name.equals("Sulfuras");
+    }
+
+    private static boolean isAgedBrie(Item item) {
+        return item.name.equals("Aged Brie");
     }
 }
