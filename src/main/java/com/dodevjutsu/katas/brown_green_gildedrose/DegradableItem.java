ed2352a6cf7e4c237853c817b4aaca193a9effed
trikitrok <trikitrok@gmail.com>
2016-05-19 19:37:34 +0200

    refactor: move updating quality code into DegradableItem

--- src/main/java/com/dodevjutsu/katas/brown_green_gildedrose/DegradableItem.java
+++ src/main/java/com/dodevjutsu/katas/brown_green_gildedrose/DegradableItem.java
@@ -1,9 +1,36 @@
 package com.dodevjutsu.katas.brown_green_gildedrose;
 
 public class DegradableItem {
+    private static final int MINIMUM_QUALITY = 0;
+    private static final int MAXIMUM_QUALITY = 50;
     private final Item item;
 
     public DegradableItem(Item item) {
         this.item = item;
+    }
+
+    public void updateQuality() {
+        if(item.name.equals("Sulfuras")){
+            return;
+        }
+
+        if(item.name.equals("Aged Brie")) {
+            increaseQualityBy(1);
+            return;
+        }
+
+        if(item.sellIn < 0) {
+            decreaseQualityBy(2);
+            return;
+        }
+        decreaseQualityBy(1);
+    }
+
+    private void increaseQualityBy(int amount) {
+        item.quality = Math.min(item.quality + amount, MAXIMUM_QUALITY);
+    }
+
+    private void decreaseQualityBy(int amount) {
+        item.quality = Math.max(item.quality - amount, MINIMUM_QUALITY);
     }
 }
--- src/main/java/com/dodevjutsu/katas/brown_green_gildedrose/GildedRose.java
+++ src/main/java/com/dodevjutsu/katas/brown_green_gildedrose/GildedRose.java
@@ -1,8 +1,6 @@
 package com.dodevjutsu.katas.brown_green_gildedrose;
 
 public class GildedRose {
-    private static final int MINIMUM_QUALITY = 0;
-    private static final int MAXIMUM_QUALITY = 50;
     private final Item item;
     private final DegradableItem degradableItem;
 
@@ -16,36 +14,15 @@ public class GildedRose {
             return;
         }
         ageItem();
-        
+
         updateItemQuality();
     }
 
     private void updateItemQuality() {
-        if(item.name.equals("Sulfuras")){
-            return;
-        }
-
-        if(item.name.equals("Aged Brie")) {
-            increaseQualityBy(1);
-            return;
-        }
-
-        if(item.sellIn < 0) {
-            decreaseQualityBy(2);
-            return;
-        }
-        decreaseQualityBy(1);
-    }
-
-    private void increaseQualityBy(int amount) {
-        item.quality = Math.min(item.quality + amount, MAXIMUM_QUALITY);
+        degradableItem.updateQuality();
     }
 
     private void ageItem() {
         item.sellIn -= 1;
-    }
-
-    private void decreaseQualityBy(int amount) {
-        item.quality = Math.max(item.quality - amount, MINIMUM_QUALITY);
     }
 }
