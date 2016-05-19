19659169edf8546cb30affb4c55afb9951c12ce7
trikitrok <trikitrok@gmail.com>
2016-05-19 20:01:43 +0200

    refactor: making room to more easily change GildedRose to have several items in its catalog

--- src/main/java/com/dodevjutsu/katas/brown_green_gildedrose/GildedRose.java
+++ src/main/java/com/dodevjutsu/katas/brown_green_gildedrose/GildedRose.java
@@ -1,15 +1,22 @@
 package com.dodevjutsu.katas.brown_green_gildedrose;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GildedRose {
     private final DegradableItem item;
+    private final List<DegradableItem> items = new ArrayList<>();
 
     public GildedRose(Item item) {
         this.item = new DegradableItem(item);
+        this.items.add(this.item);
     }
 
     public void updateInventory() {
-        item.age();
-        item.updateQuality();
+        for(DegradableItem item : items) {
+            item.age();
+            item.updateQuality();
+        }
     }
 
 }
